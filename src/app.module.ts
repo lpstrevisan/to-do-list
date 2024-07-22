5e603cbeed4419297094e03b6f8fd142c8b2162b
Luís Paulo Silva Trevisan <lpst@cin.ufpe.br>
2024-07-22 13:33:35 -0300

    add: basic crud with nest crud generator

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MembersModule } from './members/members.module';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'test',
       autoLoadEntities: true,
       synchronize: true,
-    })
+    }),
+    MembersModule
   ],
   controllers: [AppController],
   providers: [AppService],
