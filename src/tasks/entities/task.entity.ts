0c2d4866106ea353bd98a04849abc34f235545bf
Luís Paulo Silva Trevisan <lpst@cin.ufpe.br>
2024-07-22 13:58:43 -0300

    add: configuration of TypeORM in tasks module and tasks service

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,24 @@
-export class Task {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Length } from 'class-validator';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({nullable: false, length: 50})
+  @Length(5)
+  name: string;
+
+  @Column({ length: 140 })
+  description: string;
+
+  @Column({ nullable: false, default: false })
+  finished: boolean;
+
+  @Column()
+  finishDate: Date;
+
+  @Column({ nullable: false, default: 'Baixa' })
+  priority: 'Baixa' | 'Média' | 'Alta';
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
+  constructor(
+    @InjectRepository(Task)
+    private taskRepository: Repository<Task>
+  ) {}
+
   create(createTaskDto: CreateTaskDto) {
     return 'This action adds a new task';
   }
