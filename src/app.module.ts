086c5fe27a0f2ad9df09b47ec8a929aee9b86142
Luís Paulo Silva Trevisan <lpst@cin.ufpe.br>
2024-07-22 13:44:10 -0300

    add: basic crud with nest cud generator

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
@@ -14,7 +15,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'test',
       autoLoadEntities: true,
       synchronize: true,
-    })
+    }),
+    TasksModule
   ],
   controllers: [AppController],
   providers: [AppService],
